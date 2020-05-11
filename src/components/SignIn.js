dec0e32bc92e64af6a7461c25d67d9f8ccb58214
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-05-11 23:37:45 +0530

    added basic colors in base
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
-export default class SignIN extends React.Component{
-    constructor(props){
-        super(props)
-    }
+export default class SignIN extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-    
+	render() {
+		return (
+			<div className="SignIn">
+				<Button>hllo</Button>
+				<form>
+					<label for="username">
+						<b>Username</b>
+					</label>
 
-    render(){
-        return (
-            <div className ="SignIn">
-            <form>
-            <label for = "username" ><b>Username</b></label>
-            
-            <input type = "text" name = "username" />
-            
-            <label for = "password" ><b>Password</b></label>
-            
-            <input type = "password" name = "password" />
-            <div class="psw">Forgot <a href="#">password?</a></div>
-            <div className = "divbtn">
-           <button className = "lgnbtn">Continue</button>
-           </div>
-            </form>
-            </div>
-        )
-    }
+					<input type="text" name="username" />
+
+					<label for="password">
+						<b>Password</b>
+					</label>
+
+					<input type="password" name="password" />
+					<div class="psw">
+						Forgot <a href="#">password?</a>
+					</div>
+					<div className="divbtn">
+						<button className="lgnbtn">Continue</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
