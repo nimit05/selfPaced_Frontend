9bcbaaf69b0e371e1c794a75167c15f68432eefb
nimit05 <nimitwadhwa722@gmail.com>
2020-05-11 23:25:42 +0530

    building product container

---
+++ src/components/container.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Container = () => {
+        return (
+          <div>
+          <div  className = "productbox">
+          <div className = "border"></div>
+          <div className = "heart-shape"></div>
+          </div>
+          </div>
+        )
+    }
+
+
+export default Container
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './styles/style.scss'
 // import 'normalize.css/normalize.css'
 import Apiroute from './routes/Apirouter'
-// import Register from './components/Register';
+
 
 ReactDOM.render(<Apiroute />, document.getElementById('root'));
