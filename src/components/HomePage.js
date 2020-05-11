47546aaa831507caba861f9d7660734319dcba25
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-05-12 03:41:04 +0530

    added main search container
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,70 @@
 import React from 'react';
 import Header from './Header';
 import MainHeader from './MainHeader';
+import MainSearch from './MainSearch';
 
-const homePage = () => {
-	return (
-		<div>
-			<Header />
-			<MainHeader />
+class HomePage extends React.Component {
+	componentDidMount() {
+		window.addEventListener('scroll', this.Scroll);
+	}
 
-			<h1>This is PuranibOOk.com</h1>
-		</div>
-	);
-};
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.Scroll);
+	}
 
-export default homePage;
+	Scroll = () => {
+		let pos = window.scrollY;
+		if (pos > 30) {
+			let h = document.getElementsByClassName('MainHeader')[0];
+
+			h.style.backgroundImage = 'linear-gradient(#ff6600,#ff115e)';
+		} else {
+			let h = document.getElementsByClassName('MainHeader')[0];
+
+			h.style.backgroundImage = 'none';
+		}
+	};
+
+	render() {
+		return (
+			<div className="HomePage">
+				<Header />
+				<MainHeader />
+				<MainSearch />
+
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+				<h1>This is PuranibOOk.com</h1>
+			</div>
+		);
+	}
+}
+
+export default HomePage;
