6fae42ae5a245f194c08b96430a46461ab6ab135
nimit05 <nimitwadhwa722@gmail.com>
2020-05-11 14:56:55 +0530

    login page updated

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,40 +6,40 @@ export default class Register extends React.Component {
 		this.Signup = this.Signup.bind(this);
 	}
 
-	Signup() {
-		let name = document.getElementById('name').value;
-		let username = document.getElementById('username').value;
-		let email = document.getElementById('email').value;
-		let password = document.getElementById('password').value;
-		let phone_Number = document.getElementById('phone_Number').value;
-		let Address = document.getElementById('Address').value;
+	// Signup() {
+	// 	let name = document.getElementById('name').value;
+	// 	let username = document.getElementById('username').value;
+	// 	let email = document.getElementById('email').value;
+	// 	let password = document.getElementById('password').value;
+	// 	let phone_Number = document.getElementById('phone_Number').value;
+	// 	let Address = document.getElementById('Address').value;
 
-		fetch('api/register', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				user: {
-					name: name,
-					username: username,
-					email: email,
-					password: password,
-					phone_Number: phone_Number,
-					Address: Address
-				}
-			})
-		})
-			.then((res) => res.json())
-			.then((parJson) => {
-				if (parJson.email === email) {
-					alert('Registration Complete Now log in ');
-				} else if (parJson.errors) {
-					alert(parJson.errors.body);
-				}
-			})
-			.catch((error) => {
-				alert(error);
-			});
-	}
+	// 	fetch('api/register', {
+	// 		method: 'POST',
+	// 		headers: { 'Content-Type': 'application/json' },
+	// 		body: JSON.stringify({
+	// 			user: {
+	// 				name: name,
+	// 				username: username,
+	// 				email: email,
+	// 				password: password,
+	// 				phone_Number: phone_Number,
+	// 				Address: Address
+	// 			}
+	// 		})
+	// 	})
+	// 		.then((res) => res.json())
+	// 		.then((parJson) => {
+	// 			if (parJson.email === email) {
+	// 				alert('Registration Complete Now log in ');
+	// 			} else if (parJson.errors) {
+	// 				alert(parJson.errors.body);
+	// 			}
+	// 		})
+	// 		.catch((error) => {
+	// 			alert(error);
+	// 		});
+	// }
 
 	render() {
 		return (
