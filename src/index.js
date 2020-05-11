94656e4da8795a4dd7d52789575a961552ec7efe
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-05-11 15:39:04 +0530

    bugs
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './styles/style.scss'
+import './styles/style.scss';
 // import 'normalize.css/normalize.css'
 // import Apiroute from './routes/Apirouter'
 import Register from './components/Register';
