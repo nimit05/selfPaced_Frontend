8f566e692d49c7f00f107cd9ec0264a90d373284
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-05-13 03:44:39 +0530

    added googlt btn and change some layout
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- public/reg.js
+++ public/reg.js
@@ -1,68 +1,75 @@
 $(() => {
-  $('.form').find('input, textarea').on('keyup blur focus', function (e) {
+	$('.form').find('input, textarea').on('keyup blur focus', function(e) {
+		var $this = $(this),
+			label = $this.prev('label');
 
-    var $this = $(this),
-      label = $this.prev('label');
+		if (e.type === 'keyup') {
+			if ($this.val() === '') {
+				label.removeClass('active highlight');
+			} else {
+				label.addClass('active highlight');
+			}
+		} else if (e.type === 'blur') {
+			if ($this.val() === '') {
+				label.removeClass('active highlight');
+			} else {
+				label.removeClass('highlight');
+			}
+		} else if (e.type === 'focus') {
+			if ($this.val() === '') {
+				label.removeClass('highlight');
+			} else if ($this.val() !== '') {
+				label.addClass('highlight');
+			}
+		}
+	});
 
-    if (e.type === 'keyup') {
-      if ($this.val() === '') {
-        label.removeClass('active highlight');
-      } else {
-        label.addClass('active highlight');
-      }
-    } else if (e.type === 'blur') {
-      if ($this.val() === '') {
-        label.removeClass('active highlight');
-      } else {
-        label.removeClass('highlight');
-      }
-    } else if (e.type === 'focus') {
+	let isActive = true;
 
-      if ($this.val() === '') {
-        label.removeClass('highlight');
-      }
-      else if ($this.val() !== '') {
-        label.addClass('highlight');
-      }
-    }
+	$('.tab a').on('click', function(e) {
+		e.preventDefault();
 
-  });
+		$(this).parent().addClass('active');
+		$('.tabn a').parent().addClass('active');
 
-  let isActive = true;
+		$(this).parent().siblings().removeClass('active');
 
-  $('.tab a').on('click', function (e) {
+		if (isActive) {
+			$('.tabp').removeClass('active');
+			isActive = false;
+		} else {
+			isActive = true;
+		}
 
-    e.preventDefault();
+		target = $(this).attr('href');
 
-    $(this).parent().addClass('active');
-    $('.tabn a').parent().addClass('active');
+		$('.tab-content > div').not(target).hide();
 
-    $(this).parent().siblings().removeClass('active');
+		$(target).fadeIn(500);
+	});
 
-    if (isActive) {
-      $('.tabp').removeClass('active');
-      isActive = false;
-    }
-    else { isActive = true }
+	$('#submit_btn')[0].onclick = () => {
+		if ($('#pass')[0].value == $('#conPass')[0].value) {
+		} else {
+			alert('Confirm Password should not be same as Password ');
+		}
+	};
+});
 
-    target = $(this).attr('href');
-
-    $('.tab-content > div').not(target).hide();
-
-    $(target).fadeIn(600);
-
-  });
-
-
-
-  $('#submit_btn')[0].onclick = () => {
-    if ($('#pass')[0].value == $('#conPass')[0].value) { }
-    else {
-      alert("Confirm Password should not be same as Password ")
-    }
-  }
-
-
-
-
-})
+function onSuccess(googleUser) {
+	console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+	console.log(error);
+}
+function renderButton() {
+	gapi.signin2.render('my-signin2', {
+		scope: 'profile email',
+		width: 220,
+		height: 50,
+		longtitle: true,
+		theme: 'dark',
+		onsuccess: onSuccess,
+		onfailure: onFailure
+	});
+}
