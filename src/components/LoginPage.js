a01341519586620f08ee18499db68d125d594460
nimit05 <nimitwadhwa722@gmail.com>
2020-05-12 14:49:08 +0530

    back to previous login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Header from './Header';
 import SignIN from './SignIn';
 import NewUser from './NewUser';
+import pblogo from '../img/pblogo.png';
+
 
 export default class LoginPage extends React.Component {
 	render() {
@@ -20,3 +21,19 @@ export default class LoginPage extends React.Component {
 		);
 	}
 }
+
+const Header = () => {
+	return (
+		<div className="header_cont">
+			<div className="header_left">
+				<img src={pblogo} alt=" " />
+				<b>PuraniBooks</b>
+				<span>.com</span>
+			</div>
+
+			<div className="header_center">
+				<span>Get Upto 50% off </span>
+			</div>
+		</div>
+	);
+};
