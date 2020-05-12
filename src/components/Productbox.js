1d614a54d20d34f2b41bfa81e996f927554747cb
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-05-12 22:42:44 +0530

    minor changes
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- src/components/Productbox.js
+++ src/components/Productbox.js
@@ -1,31 +1,55 @@
 import React from 'react';
 import bookcover from '../img/bookcover.jpg';
 
-// <div className = "condition">Condition : <span className = "good">{this.state.isgood ?  "good" : "poor" } </span></div>
+// props to send are {1 title 2 tag 3 bookimg 4 stitle 5 short_des 6 price}
 
 export default class Productbox extends React.Component {
+	addToCart = () => {
+		// to do req to add to cart
+
+		this.setState((prev) => {
+			return {
+				addedToCart: !prev.addedToCart
+			};
+		});
+	};
+
 	constructor(props) {
 		super(props);
 		this.state = {
-			isgood: true,
-			old: true
+			addedToCart: false
 		};
 	}
+
 	render() {
+		let c;
+
+		if (this.state.addedToCart) {
+			c = {
+				backgroundColor: 'green'
+			};
+		} else {
+			c = {
+				backgroundColor: '#fa255e'
+			};
+		}
+
 		return (
 			<div className="productcont">
 				<div className="tag">
-					<strong>OLD</strong>
+					<strong>{this.props.tag}</strong>
 				</div>
 				<div className="product_img">
 					<img className="bookcover" src={bookcover} alt=" " />
 				</div>
 				<div className="product_body">
-					<h3>H.C. VERMA</h3>
-					<h6>(E-book)</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-					<h1>₹50</h1>
-					<button className="add_to_cart_btn">Add to Cart</button>
+					<h3>{this.props.title}</h3>
+					<h6>({this.props.stitle})</h6>
+					<p>{this.props.short_des}</p>
+					<h1>{this.props.price}</h1>
+					<button onClick={this.addToCart} className="add_to_cart_btn" style={c}>
+						{this.state.addedToCart ? 'Added To Cart' : 'Add To Cart'}
+					</button>
 				</div>
 			</div>
 		);
