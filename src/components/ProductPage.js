23f86e4a3f16cc3de879e2d7a95773f1b019d6e1
nimit05 <nimitwadhwa722@gmail.com>
2020-05-16 20:04:39 +0530

    product page

---
+++ src/components/ProductPage.js
@@ -0,0 +1,73 @@
+import React from 'react'
+// import Header from './Header'
+// import MainHeader from './MainHeader'
+import cart from '../img/cart.svg';
+import down from '../img/down.svg';
+import pblogo from '../img/pblogo.png';
+
+export default class ProductPage extends React.Component{
+    constructor(props){
+        super(props)
+        this.ListItem = this.ListItem.bind(this)
+    }
+    ListItem(){
+        return(
+            <div>
+            <ul>
+            <li>Hello world</li>
+            </ul>
+            </div>
+        )
+    }
+    render(){
+        return(
+            <div>
+            <Header ListItem = {ListItem} />
+            <MainHeader />
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+            <div className="header_left_PP">
+            <img src={pblogo} alt=" " />
+            <b>PuraniBooks</b>
+            <span>.com</span>
+        </div>
+            </div>
+        )
+    }
+}
+
+const MainHeader = (props) => {
+	
+		return (
+            
+            <div className="MainHeader_PP">
+				<div className="cat_btn_con frse">
+					<button id="cat_btn_drop" onClick = {props.ListItem} >
+						<span>CATEGORIES</span> <img src={down} alt="" />
+					</button>
+					<button className="bestseller_btn">Best Seller</button>
+					<button className="new_books_btn">New Books</button>
+					<button className="old_books_btn">Old Books</button>
+					<button className="ebooks_btn">E-books</button>
+				</div>
+				<div className="user_btn_con frse">
+					<img src={cart} alt=" " />
+					<button id="login_btn" onClick = {() => {
+						window.location.href = "/login.html";
+
+					}}>Login</button>
+					<button id="reg_btn" onClick = {() => {
+                 window.location.href = "/regis.html";
+
+					}}>Register</button>
+				</div>
+			</div>
+		);
+	}
