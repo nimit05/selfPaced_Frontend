501d4d45fa4b827595b687b99149362ff6d6073b
nimit05 <nimitwadhwa722@gmail.com>
2020-05-26 12:11:44 +0530

    login connected to backend

--- public/login.js
+++ public/login.js
@@ -1,3 +1,22 @@
+async function postData(url = '', data = {}) {
+	// Default options are marked with *
+	const response = await fetch(url, {
+		method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		mode: 'cors', // no-cors, *cors, same-origin
+		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: 'same-origin', // include, *same-origin, omit
+		headers: {
+			'Content-Type': 'application/json'
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: 'follow', // manual, *follow, error
+		referrerPolicy: 'no-referrer', 
+		body: JSON.stringify(data) // body data type must match "Content-Type" header
+	});
+	return response.json(); // parses JSON response into native JavaScript objects
+}	
+
+
 $(() => {
 	let isActive = true;
 
@@ -43,3 +62,63 @@ function renderButton() {
 		onfailure: onFailure
 	});
 }
+let nologmistake = false;
+function log_username_vali() {
+	if ($('#log_username').val().length == 0) {
+		$('#below_logUser').append($('<p>')).text('Enter your username');
+		$('#log_username').css({
+			border: 'red solid',
+			'border-width': 'thin'
+		});
+		nologmistake = false;
+	} else {
+		$('#below_logUser').empty();
+		$('#log_username').css({
+			border: '1px solid #1eff00'
+		});
+		nologmistake = true;
+	}
+}
+
+function log_pass_vali() {
+	if ($('#wrong_pass').val().length == 0) {
+		$('#below_logPass').append($('<p>')).text('Enter your password');
+		$('#wrong_pass').css({
+			border: 'red solid',
+			'border-width': 'thin'
+		});
+		nologmistake = false;
+	} else {
+		$('#below_logUser').empty();
+		$('#wrong_pass').css({
+			border: '1px solid #1eff00'
+		});
+		nologmistake = true;
+	}
+}
+
+function log_details(){
+	let data = {
+		user :{
+			username : $('#log_username').val(),
+            password : $('#wrong_pass').val()
+		}
+	}
+  
+	if(nologmistake){
+	postData('/api/login/' , data).then((data) => {
+		if(data.username){
+			location.replace('/')
+		}else if(data.error){
+			$('#below_logUser').append($('<p>')).text('Username Or Password is Incorrect');
+			$('#log_username').css({
+				border: 'red solid',
+				'border-width': 'thin'
+			});
+		}
+	})
+}
+   nologmistake = false;
+
+}
+
