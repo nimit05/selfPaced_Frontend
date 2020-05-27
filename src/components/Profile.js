0022a9e4b1de3746217fde17378bc9cb8b175bab
nimit05 <nimitwadhwa722@gmail.com>
2020-05-27 12:00:54 +0530

    edited and save function added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -24,15 +24,13 @@ class ProfileCard extends React.Component{
 		fetch('/api/user', {
 			method: 'GET', // *GET, POST, PUT, DELETE, etc.
 			mode: 'cors', // no-cors, *cors, same-origin
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			cache: 'no-cache', 
 			credentials: 'same-origin', // include, *same-origin, omit
 			headers: {
 				'Content-Type': 'application/json'
-				// 'Content-Type': 'application/x-www-form-urlencoded',
 			},
 			redirect: 'follow', // manual, *follow, error
-			referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-			// body: JSON.stringify(data) // body data type must match "Content-Type" header
+			referrerPolicy: 'no-referrer' 
 		})
 			.then((res) => res.json())
 			.then((data) => {
@@ -56,19 +54,38 @@ class ProfileCard extends React.Component{
                 phone_Number : null,
                 Address : null,
                 name : null,
-                coins : null 
+                coins : null ,
+                edited : false
             }
         }
-
+     
+         
     render(){
     return(
-        <div className = "ProfileCard">
+        <div id = "ProfileCard">
          <div className = "info">
              <div className = "user_pro_pic">    
             </div>
             <div className ="user_name">
                 {this.state.name} 
-                 <div className = "edit">Edit profile</div>
+                 <div className = "edit" onClick = {() => {
+                     this.setState((prevState) => {
+                         return{
+                             edited : !prevState.edited
+                         } 
+                     })
+          
+                        document.getElementById('ProfileCard').style.height = '40vw'
+                        
+                        if(this.state.edited){
+                        document.getElementById('ProfileCard').style.height = '32vw'
+
+                        }
+                    
+                 }} > {this.state.edited ? 'Cancel':'Edit Profile'}
+                  
+                 
+                 </div>
              </div>
         </div>
             <div className = "hr_pp">
@@ -77,28 +94,43 @@ class ProfileCard extends React.Component{
             <div className="details">
                 <div className = "heading">
                     Username
-                    <div className = "heading_value">
+                         {this.state.edited ? (<input type = "text" className = "heading_value " value = {this.state.username}  />)
+                          : (<div className = "heading_value">
                          {this.state.username}
-                    </div>
-                </div>
+                         </div>
+                         )}
+                        
+                 </div>
                 <div className = "heading">
                     Email
                   <div className = "heading_value">
-                        {this.state.email}
+                  {this.state.edited ? (<input type = "text"  className = "heading_value input_email" id = "input_email" />)
+                   : (<div className = "heading_value">
+                  {this.state.email}
+                  </div>
+                  )}
                          
                 </div>
                 </div>
                 <div className = "heading">
                     Phone No.
                     <div className = "heading_value">
-                         {this.state.phone_Number}
+                    {this.state.edited ? (<input type = "text"  className = "heading_value "  id = "input_phone_Number" />) 
+                    : (<div className = "heading_value">
+                    {this.state.phone_Number}
+                    </div>
+                    )}
                         
                   </div>
                 </div>
                 <div className = "heading">
                     Address
                 <div className = "heading_value">
-                        {this.state.Address}
+                {this.state.edited ? (<input type = "text"  className = "heading_value input_Address" id = "input_Address" />) 
+                : (<div className = "heading_value">
+                {this.state.Address}
+                </div>
+                )}
                     
                      </div>
                 </div>
@@ -108,6 +140,30 @@ class ProfileCard extends React.Component{
                      {this.state.coins} coins
                     
                         </div>
+                </div>
+                <div className = "heading save" onClick = {() => {
+                    this.setState(() => {
+                        return{
+                            edited : false
+                        }
+                    })
+                    document.getElementById('ProfileCard').style.height = '32vw'
+                    let data = {
+                        email :document.getElementById('input_email').value,
+                        phone_Number : document.getElementById('input_phone_Number').value,
+                        Address : document.getElementById('input_Address').value
+
+                    }
+                      
+                    postData('api/user/' , data).then((data) => {
+                        if(data.email){
+                            window.location.reload()
+                        }else{
+                            alert('we are having some problem')
+                        }
+                    })
+                }} >
+                 {this.state.edited ? 'Save' : ''}
                 </div>
                 <div className = "log_out">
                 Log Out
@@ -128,4 +184,22 @@ const Items_Cont = () => {
            
         </div>
     )
+}
+
+async function postData(url = '', data = {}) {
+	// Default options are marked with *
+	const response = await fetch(url, {
+		method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+		mode: 'cors', // no-cors, *cors, same-origin
+		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: 'same-origin', // include, *same-origin, omit
+		headers: {
+			'Content-Type': 'application/json'
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: 'follow', // manual, *follow, error
+		referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		body: JSON.stringify(data) // body data type must match "Content-Type" header
+	});
+	return response.json(); // parses JSON response into native JavaScript objects
 }
