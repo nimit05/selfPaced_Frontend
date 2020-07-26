04f80137e7dca1c9e4cd9dd0e1c6fbad8b9f1cce
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-07-26 22:53:51 +0530

    bugs fixed
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- src/containers/Library.js
+++ src/containers/Library.js
@@ -1,60 +1,61 @@
-import React from 'react';
-import MyProBox from '../components/MyProBox';
-import Header from '../components/Header';
-import Base_Header from '../Hooks/Base_header';
+import React from "react";
+import MyProBox from "../components/MyProBox";
+import Header from "../components/Header";
+import Base_Header from "../Hooks/Base_header";
 // import PDFViewer from 'pdf-viewer-reactjs';
 
 class Library extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			products: []
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
 
-		fetch('api/products/myproducts')
-			.then((res) => {
-				console.log('hii ' + res);
-				return res.json();
-			})
-			.then((data) => {
-				console.log(data);
-				this.setState(() => {
-					return {
-						products: data
-					};
-				});
-			});
-	}
+    fetch("api/products/myproducts")
+      .then(res => {
+        console.log("hii " + res);
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.setState(() => {
+          return {
+            products: data
+          };
+        });
+      });
+  }
 
-	render() {
-		return (
-			<div className="Library_con">
-				{/* <PDFViewer
+  render() {
+    return (
+      <div className="Library_con">
+        {/* <PDFViewer
 					document={{
 						url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf'
 					}}
 				/> */}
-				<h1 id="lib_head">Shelf ({this.state.products.length})</h1>
+        <h1 id="lib_head">Shelf ({this.state.products.length})</h1>
 
-				<div className="proCon">
-					{this.state.products &&
-						this.state.products.map((e, i) => {
-							return (
-								<MyProBox
-									key={i}
-									tag={e.Product.tag}
-									title={e.Product.title}
-									bookimg={`/covers/${e.Product.cover_img}`}
-									file={`/files/${e.Product.product_file}`}
-									refId={e.Product.refrenceId}
-								/>
-							);
-						})}
-				</div>
-				<Base_Header />
-			</div>
-		);
-	}
+        <div className="proCon">
+          {this.state.products &&
+            this.state.products.map((e, i) => {
+              return (
+                <MyProBox
+                  key={i}
+                  productId={e.Product.id}
+                  tag={e.Product.tag}
+                  title={e.Product.title}
+                  bookimg={`/covers/${e.Product.cover_img}`}
+                  file={`/files/${e.Product.product_file}`}
+                  refId={e.Product.refrenceId}
+                />
+              );
+            })}
+        </div>
+        <Base_Header />
+      </div>
+    );
+  }
 }
 
 export default Library;
