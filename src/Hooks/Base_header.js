33c73f729087afd25b41973659bf93c29eaaa95e
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-07-28 20:04:33 +0530

    added new HomePage
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- src/Hooks/Base_header.js
+++ src/Hooks/Base_header.js
@@ -1,114 +1,96 @@
-import React from 'react';
-import home_icon from '../img/icon.svg';
-import cart from '../img/cart.svg';
-import plus from '../img/plus.svg';
-import lib from '../img/lib.svg';
-import picpro from '../img/propic.svg';
-import { GoogleLogin } from 'react-google-login';
+import React from "react";
+import home_icon from "../img/icon.svg";
+import cart from "../img/cart.svg";
+import plus from "../img/plus.svg";
+import lib from "../img/lib.svg";
+import picpro from "../img/propic.svg";
+import { GoogleLogin } from "react-google-login";
+import { Link } from "react-router-dom";
 
 export default class Base_Header extends React.Component {
-	responseGoogle = (response) => {
-		fetch('/api/register/google', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ tokenId: response.tokenId })
-		})
-			.then((res) => res.json())
-			.then((parJson) => {
-				if (parJson.email) {
-					window.location.href = '/';
-				}
-			});
-	};
+  responseGoogle = response => {
+    fetch("/api/register/google", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ tokenId: response.tokenId })
+    })
+      .then(res => res.json())
+      .then(parJson => {
+        if (parJson.email) {
+          window.location.href = "/";
+        }
+      });
+  };
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		fetch('/api/user').then((res) => res.json()).then((data) => {
-			console.log(data);
-			if (data) {
-				this.setState(() => {
-					return {
-						islogin: true
-					};
-				});
-			}
-		});
+    fetch("/api/user")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data) {
+          this.setState(() => {
+            return {
+              islogin: true
+            };
+          });
+        }
+      });
 
-		this.state = {
-			islogin: false
-		};
-	}
+    this.state = {
+      islogin: false
+    };
+  }
 
-	render() {
-		return (
-			<div className="base_header">
-				<div className="img_cont_bh">
-					{this.state.islogin ? (
-						<div className="icon_bh">
-							{' '}
-							<img
-								onClick={() => {
-									window.location.href = '/';
-								}}
-								className="home_svg "
-								src={home_icon}
-								alt=" "
-							/>
-							<img
-								onClick={() => {
-									window.location.href = '/myCart';
-								}}
-								src={cart}
-								alt=" "
-							/>
-							<img
-								onClick={() => {
-									window.location.href = '/sell-your-product';
-								}}
-								src={plus}
-								alt=" "
-							/>
-							<img
-								onClick={() => {
-									window.location.href = '/My-Library';
-								}}
-								src={lib}
-								alt=" "
-							/>
-							<img
-								onClick={() => {
-									window.location.href = '/myprofile';
-								}}
-								src={picpro}
-								alt=" "
-							/>
-						</div>
-					) : (
-						<div className="reg_btn_div">
-							<button
-								id="reg_btn_bh"
-								onClick={() => {
-									window.location.href = '/regis.html';
-								}}
-							>
-								Register/Login
-							</button>
-							<GoogleLogin
-								clientId="1059529825547-aplj58mp67dhb9j5smat7g5jjf3flgl2.apps.googleusercontent.com"
-								buttonText="Continue with Google"
-								style={{ backgroundColor: 'blue' }}
-								className="gbtn"
-								onSuccess={this.responseGoogle}
-								onFailure={() => {
-									alert('Error in google login ');
-								}}
-								cookiePolicy={'single_host_origin'}
-							/>
-						</div>
-					)}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="base_header">
+        <div className="img_cont_bh">
+          {this.state.islogin ? (
+            <div className="icon_bh">
+              {" "}
+              <Link to="/">
+                <img className="home_svg " src={home_icon} alt=" " />
+              </Link>
+              <Link to="/myCart">
+                <img src={cart} alt=" " />
+              </Link>
+              <Link to="/sell-your-product">
+                <img src={plus} alt=" " />
+              </Link>
+              <Link to="/My-Library">
+                <img src={lib} alt=" " />
+              </Link>
+              <Link to="/myprofile">
+                <img src={picpro} alt=" " />
+              </Link>
+            </div>
+          ) : (
+            <div className="reg_btn_div">
+              <button
+                id="reg_btn_bh"
+                onClick={() => {
+                  window.location.href = "/regis.html";
+                }}
+              >
+                Register/Login
+              </button>
+              <GoogleLogin
+                clientId="1059529825547-aplj58mp67dhb9j5smat7g5jjf3flgl2.apps.googleusercontent.com"
+                buttonText="Continue with Google"
+                style={{ backgroundColor: "blue" }}
+                className="gbtn"
+                onSuccess={this.responseGoogle}
+                onFailure={() => {
+                  alert("Error in google login ");
+                }}
+                cookiePolicy={"single_host_origin"}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
---
+++ src/components/ShopByCate.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import comp_img from "../img/brain.svg";
+import ee_img from "../img/electric-motor.svg";
+import civil_img from "../img/civil.svg";
+import ece_img from "../img/circuit.svg";
+import mech_img from "../img/motor.svg";
+import chem_img from "../img/research.svg";
+import arrow from "../img/next.svg";
+
+export default class ShopByCate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="shop_by_cate_con">
+        <h1 className="sbc_head">Shop By Category</h1>
+        <img className="arrow_right" src={arrow} alt="" />
+        <img className="arrow_left" src={arrow} alt="" />
+        <div className="sbc_body_con">
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={comp_img} alt="" />
+            <h3>Computer Science Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={ee_img} alt="" />
+            <h3>Electrical Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={civil_img} alt="" />
+            <h3>Civil Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={chem_img} alt="" />
+            <h3>Chemical Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={comp_img} alt="" />
+            <h3>Computer Science Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={ee_img} alt="" />
+            <h3>Electrical Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={civil_img} alt="" />
+            <h3>Civil Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={chem_img} alt="" />
+            <h3>Chemical Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={comp_img} alt="" />
+            <h3>Computer Science Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={ee_img} alt="" />
+            <h3>Electrical Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={civil_img} alt="" />
+            <h3>Civil Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={chem_img} alt="" />
+            <h3>Chemical Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={mech_img} alt="" />
+            <h3>Mechanical Engineering</h3>
+          </div>
+          <div className="sbc_cate">
+            {" "}
+            <img className="sbc_cate_img" src={ece_img} alt="" />
+            <h3>Electronics Engineering</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
