77aeb5a9ae11ef3885d6810bd3bf29e5fdc6b416
nimit05 <nimitwadhwa722@gmail.com>
2020-08-12 11:48:52 +0530

    width chnaged

--- public/reg.js
+++ public/reg.js
@@ -10,7 +10,7 @@ async function postData(url = '', data = {}) {
 			// 'Content-Type': 'application/x-www-form-urlencoded',
 		},
 		redirect: 'follow', // manual, *follow, error
-		referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		referrerPolicy: 'no-referrer',
 		body: JSON.stringify(data) // body data type must match "Content-Type" header
 	});
 	return response.json(); // parses JSON response into native JavaScript objects
